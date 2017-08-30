38109cf2750885ad2a7070ef84cc3e2b936db33e
jyotiprakashparida <31472189+jyotiprakashparida@users.noreply.github.com>
2017-08-31 00:18:19 +0530

    Create lab1 q2.cpp

---
+++ lab1 q2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int number1,number2,sum;
+	
+	cout<<"enter first integer:";
+	cin>>number1;
+	
+	cout<<"enter second integer:";
+	cin>>number2;
+	
+	sum=number1+number2;
+	getchar();
+	
+	cout<<"sum int"<<sum<<endl;
+	return 0;
+	
+}
